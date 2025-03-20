0ee83bb95503dde33489dc94e0f65e6ee922e0b7
Hiago Gabriel <117479227+HgGabriel@users.noreply.github.com>
2025-03-20 00:44:16 +0000

    implement HomeBanner component with mock data and styles

--- src/components/HomeBanner/HomeBanner.tsx
+++ src/components/HomeBanner/HomeBanner.tsx
@@ -1,60 +1,123 @@
 import React from "react";
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./HomeBanner.module.scss";
 import "swiper/swiper-bundle.css";
 
 const HomeBanner: React.FC = () => {
 
-  const slides = [
+  interface Step {
+    id: number;
+    stepNumber: number;
+    title: string;
+    description: string;
+  }
+  // recipes interface
+  interface Recipe {
+    id: number;
+    title: string;
+    description: string;
+    image: string;
+    tags: string[];
+    ingredients: string[];
+    steps: Step[];
+  }
+
+  const mockData: Recipe[] = [
     {
       id: 1,
-      title: "Receitas",
-      description: "Receitas para todos os gostos",
-      image: "/banner1.jpg",
+      title: "Sushi Tradicional",
+      description: "Uma combinação perfeita de arroz temperado e ingredientes frescos, preparados com maestria para oferecer uma experiência autêntica da culinária japonesa. Desfrute de opções clássicas como nigiri, delicadas fatias de peixe sobre arroz, sashimi, cortes selecionados de peixe fresco, e makis, rolinhos envoltos em alga nori, recheados com peixe, frutos do mar ou vegetais.",
+      image: "https://images.unsplash.com/photo-1617196905100-216ffe128142?q=80&w=1164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      tags: ["tag1", "tag2"],
+      ingredients: ["ingredient1", "ingredient2"],
+      steps: [
+        {
+          id: 1,
+          stepNumber: 1,
+          title: "Step 1",
+          description: "Description 1",
+        },
+        {
+          id: 2,
+          stepNumber: 2,
+          title: "Step 2",
+          description: "Description 2",
+        },
+      ]
     },
     {
       id: 2,
-      title: "Receitas",
-      description: "Receitas para todos os gostos",
-      image: "/banner2.jpg",
+      title: "Sashimi",
+      description: "Uma combinação perfeita de arroz temperado e ingredientes frescos, preparados com maestria para oferecer uma experiência autêntica da culinária japonesa. Desfrute de opções clássicas como nigiri, delicadas fatias de peixe sobre arroz, sashimi, cortes selecionados de peixe fresco, e makis, rolinhos envoltos em alga nori, recheados com peixe, frutos do mar ou vegetais.",
+      image: "https://images.unsplash.com/photo-1583623025817-d180a2221d0a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      tags: ["tag1", "tag2"],
+      ingredients: ["ingredient1", "ingredient2"],
+      steps: [
+        {
+          id: 1,
+          stepNumber: 1,
+          title: "Step 1",
+          description: "Description 1",
+        },
+        {
+          id: 2,
+          stepNumber: 2,
+          title: "Step 2",
+          description: "Description 2",
+        },
+      ]
     },
     {
       id: 3,
-      title: "Receitas",
-      description: "Receitas para todos os gostos",
-      image: "/banner3.jpg",
-    },
-    {
-      id: 4,
-      title: "Receitas",
-      description: "Receitas para todos os gostos",
-      image: "/banner4.jpg",
-    },
-    {
-      id: 5,
-      title: "Receitas",
-      description: "Receitas para todos os gostos",
-      image: "/banner5.jpg",
-    },
-  ];
+      title: "Lámen Japonês",
+      description: "Uma combinação perfeita de arroz temperado e ingredientes frescos, preparados com maestria para oferecer uma experiência autêntica da culinária japonesa. Desfrute de opções clássicas como nigiri, delicadas fatias de peixe sobre arroz, sashimi, cortes selecionados de peixe fresco, e makis, rolinhos envoltos em alga nori, recheados com peixe, frutos do mar ou vegetais.",
+      image: "https://images.unsplash.com/photo-1618889482923-38250401a84e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      tags: ["tag1", "tag2"],
+      ingredients: ["ingredient1", "ingredient2"],
+      steps: [
+        {
+          id: 1,
+          stepNumber: 1,
+          title: "Step 1",
+          description: "Description 1",
+        },
+        {
+          id: 2,
+          stepNumber: 2,
+          title: "Step 2",
+          description: "Description 2",
+        },
+      ]
+    }
+  ]
 
   return (
     <>
       <Swiper
-        pagination={true}
-        modules={[Pagination]}
+        pagination={{
+          clickable: true,
+
+        }}
+        modules={[Pagination, Autoplay]}
         className={styles.mySwiper}
         slidesPerView={1}
-        spaceBetween={30}
+        loop={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: true,
+        }}
       >
-        {slides.map((slide) => (
-          <SwiperSlide key={slide.id}>
-            <div className={styles.slide}>
+        {mockData.map((slide) => (
+          <SwiperSlide key={slide.id} className={styles.swiperSlide}>
+            <div className={styles.slide} >
               <div className={styles.slideContent}>
                 <h2>{slide.title}</h2>
+                <img src="https://cdn.pixabay.com/photo/2022/01/11/12/16/rating-6930474_1280.png" alt="rating" className={styles.rating} />
                 <p>{slide.description}</p>
+                <a>Mais Detalhes</a>
               </div>
+              <img src={slide.image} alt={slide.title} className={styles.slideImage} />
             </div>
           </SwiperSlide>
         ))}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import HomeBanner from '../../components/HomeBanner/HomeBanner';
+import styles from './Home.module.scss';
 
 const Home: React.FC = () => {
   return (
     <>
-    <HomeBanner/>
-    <div>
-      
-    </div>
+      <HomeBanner />
+      <div className={styles.trendingContainer}>
+        <h1>
+          Destaques
+        </h1>
+        <div className={styles.trending}>
+          <div className={styles.mainTrendingCard}>
+            <img src="https://images.unsplash.com/photo-1617196905100-216ffe128142?q=80&w=1164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Sushi Tradicional" />
+            <div className={styles.mainTrendingCardInfo}>
+              <h2>Sushi Tradicional</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
